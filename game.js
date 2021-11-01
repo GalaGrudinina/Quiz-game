05ada17b0583cd4df51e871b737a8f99bf44c12d
GalaGrudinina <92998083+GalaGrudinina@users.noreply.github.com>
2021-11-01 11:13:18 +0200

    Update game.js

--- game.js
+++ game.js
@@ -55,7 +55,7 @@ getNewQuestion = () => {
     return window.location.assign("/end.html");
   }
   questionCounter++;
-  progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
+ progressText.innerText = `Question ${questionCounter}/${MAX_QUESTIONS}`;
   //Update the progress bar
   progressBarFull.style.width = `${(questionCounter / MAX_QUESTIONS) * 100}%`;
 
@@ -95,7 +95,6 @@ choices.forEach(choice => {
     }, 1000);
   });
 });
-
 incrementScore = num => {
   score += num;
   scoreText.innerText = score;
